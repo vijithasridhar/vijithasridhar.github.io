a8e1479173139b0c20af150e1a11a16c5d3f6850
Vijitha Sridhar <vijitha.sridhar@gmail.com>
2015-08-18 01:56:30 -0700

    homepage changed

--- js/index.js
+++ js/index.js
@@ -10,9 +10,9 @@ $('document').ready(function(){
     	text.style.strokeDashoffset = length;
     	text.getBoundingClientRect();
 
-    	text.style.WebkitTransition = text.style.transition = 'stroke-dashoffset 4s ease-in-out, fill 4s ease-out';
+    	text.style.WebkitTransition = text.style.transition = 'fill 5s ease-out, stroke-dashoffset 4s ease-in-out';
 
-    	text.style.fill = 'black';    			//change back if you want the line thicker
+    	text.style.fill = '#505050';    			//change back if you want the line thicker
     	text.style.strokeDashoffset = '0';
     }
 
